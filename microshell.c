bcac3ecc0cc1a0443671468ebd5a5d305d6f1435
Spaghettificated <lescza@st.mu.edu.pl>
2025-02-07 00:59:00 +0100

    handling flags -> flag -a in ls

--- microshell.c
+++ microshell.c
@@ -211,17 +211,79 @@ int move_path(char *from, char *path){
 }
 #endif
 
+#ifndef flags
+typedef struct flag_info{
+    int flagc;
+    char *c_flags;
+    char *s_flags
+}flag_info;
+// 0 - not flag, 1 - short flag, 2 - long flag
+int flag_type(char *str){
+    if (str[0] == '-'){
+        if (str[1] == '-'){
+            return 2;
+        }
+        return 1;
+    }
+    return 0;
+}
+int is_flag(char *str, int type, char c_flag, char* s_flag){
+    if (type == 1 && c_flag != NULL && strchr(str, c_flag) != NULL){
+        return 1;
+    }
+    if (type == 2 && s_flag != NULL && strcmp(str+2, s_flag)){
+        return 1;
+    }
+    return 0;
+}
+int get_flags(char *str, char *c_flags, char *s_flags[]){
+    int type = flag_type(str);
+    if (type == 0) return 0;
+    int n = type==1 ? strlen(str)-1 : 1;
+    int out = 0;
+    for(int i = 0; c_flags[i] != '\0'; i++){
+        if (n<=0) break;
+        if (
+            (type == 1 && strchr(str+1, c_flags[i]) != NULL)
+            || (s_flags != NULL && s_flags[i] != NULL 
+                && type == 2 && !strcmp(s_flags[i], str+2)) 
+        ){
+            out |= 1<<i;
+            n--;
+        }
+    }
+    out = n<=0 ? out : 0;
+    return out;
+}
+#endif
+
 #ifndef built_ins
-void ls(commandArgs command, char *cursor, streams streams){
+void ls_once(char *path, int flags, streams streams){
     DIR *dir;
     struct dirent *entry; // https://stackoverflow.com/questions/4204666/how-to-list-files-in-a-directory-in-a-c-program
-    dir = opendir(cursor);
+    dir = opendir(path);
     if (dir) {
         while ((entry = readdir(dir)) != NULL) {
+            if (!(flags & 1) && entry->d_name[0]=='.'){ // -a flag
+                continue;
+            }
             fprintf(streams.out, "%s\n", entry->d_name);
         }
         closedir(dir);
     }
+}
+void ls(commandArgs command, char *cursor, streams streams){
+    int flags = 0;
+    int n = 0;
+    for(int i = 0; i < command.argc; i++){
+        int f = get_flags(command.argv[i], "a", (char*[]){"all",});
+        flags |= f;
+        if (f==0)
+            n++;
+    }
+    char path[STR_BUFOR_SIZE];
+    strcpy(path, cursor);
+    ls_once(path, flags, streams);
 }
 int cd(commandArgs command, char *cursor, streams streams){
     if(command.argc<=1) return 1;
@@ -630,6 +692,9 @@ int input_char(typingField *field, int hlen){
 #endif
 
 int main() {
+    printf("%d\n", flag_type("-b") );
+    char *a[] = {"a","b","c"}; 
+    printf("%d\n", get_flags("--a", "abc", (char*[]){"a","b","n"} ) );
     int hlen = histlen();
     printf(getenv("HOME"));
     long size = pathconf(".", _PC_PATH_MAX); // https://pubs.opengroup.org/onlinepubs/007904975/functions/getcwd.html
